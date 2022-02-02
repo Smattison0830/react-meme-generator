caacf2b544cdda6989fb3931547f9bb6ffe0ba58
Spencer Mattison <spenceramattison@gmail.com>
2022-02-02 19:51:00 +0900

    created header component and added simple css

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function Header() {
+    return (
+        <div className="header">
+            <img alt="" src="./images/troll-face.png" className="header--image"></img>
+            <div className="header--title">Meme Generator</div>
+            <div className="header--text">React Course - Project 3</div>
+        </div>
+    )
+}
